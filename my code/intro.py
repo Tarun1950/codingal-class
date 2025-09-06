6b38fd156d18436d60a6c1a6d677cced6bd2b048
Tarun1950 <tarunsingh3812@gmail.com>
2025-09-06 18:53:08 +0530

    datatype

---
+++ my code/intro.py
@@ -0,0 +1,13 @@
+name='tarun'
+print(name,'nice to met you')
+# Activities-1
+X='TARUN' 
+print(X)
+print(type(X))
+
+# Activities-2
+print("\n typecasting", type(float(3)))
+# Activities-3
+word='python'
+print(word)
+print(word[::-1])
