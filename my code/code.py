9b789d4c0fb229a0032194dfb6b96403b742e759
Tarun1950 <tarunsingh3812@gmail.com>
2025-09-09 19:27:12 +0530

    opeator

---
+++ my code/code.py
@@ -0,0 +1,28 @@
+# activty 1
+tree1=98
+tree2=94
+tree3=41
+tree4=96
+tree5=11
+sum=tree1+tree2+tree3+tree4+tree5
+print("SUM :",sum)
+
+average= sum/5
+print("AVERAGE :",average)
+# Activity 2
+amount=int(input("Enter the amount: "))
+note_1=amount //100
+note_2=(amount%100)//50
+note_3=((amount%100)%50)//10
+print("100 notes :",note_1)
+print("50 notes :",note_2)
+print("10 notes :",note_3)
+# Activity 3
+math=40
+science=70
+hindi=50
+english=60
+sum=math+science+hindi+english
+print("SUM :",sum)
+percentage= sum/400*100
+print("PERCENTAGE :",percentage)
